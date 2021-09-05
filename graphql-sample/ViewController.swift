ede2399757d3759cde3f18827957b9973b081b2f
Reza Daneshvar <rdaneshvar95@gmail.com>
2021-09-05 16:21:11 +0200

    xcode project created

---
+++ graphql-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  graphql-sample
+//
+//  Created by Reza Daneshvar on 05/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
